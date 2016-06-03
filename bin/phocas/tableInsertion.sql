6561966a27365f9a9aec41d745679cdf6ce27871
RockLira <shiqi.rock.luo@gmail.com>
2016-06-03 11:08:54 -0700

    removed private info

--- bin/phocas/tableInsertion.sql
+++ bin/phocas/tableInsertion.sql
@@ -108,3 +108,90 @@ insert into allOrder values
 insert into allOrder values
 (10, 5, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 1.75, 'finished', 11);
 
+insert into inStoreOrder values(1);
+insert into inStoreOrder values(2);
+insert into inStoreOrder values(3);
+insert into inStoreOrder values(4);
+insert into inStoreOrder values(5);
+
+insert into onlineOrder values(6, '8295 Scott Road', 'Simon',  6045079393);
+insert into onlineOrder values(7, '579 Yonge Street', 'Claire',  6473442637);
+insert into onlineOrder values(8, '13 Baldwin Street', 'Marcus',  4167928858);
+insert into onlineOrder values(9, '120 Lombard Avenue', 'Sameer',  6478961774);
+insert into onlineOrder values(10, '92 Front Street E', 'Ruth',  4163927219);
+
+insert into orders values(1, 'hashbrown');
+insert into orders values(2, 'cheeseburger');
+insert into orders values(3, 'coke');
+insert into orders values(4, 'fries');
+insert into orders values(5, 'tuna');
+
+insert into serves values(1, 'hashbrown');
+insert into serves values(6, 'cheeseburger');
+insert into serves values(11, 'coke');
+insert into serves values(7, 'fries');
+insert into serves values(8, 'tuna');
+
+insert into delivery values
+(1, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 'delivered');
+insert into delivery values
+(2, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 'delivered');
+insert into delivery values
+(3, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 'delivered');
+insert into delivery values
+(4, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 'delivered');
+insert into delivery values
+(5, TO_DATE('22/April/2016 8:30:00AM','DD/MON/YY HH:MI:SSAM'), 'delivered');
+
+insert into deliveryHasItems values
+(1, 'cheeseburger');
+insert into deliveryHasItems values
+(2, 'chocolate pudding');
+insert into deliveryHasItems values
+(3, 'chocolate pudding');
+insert into deliveryHasItems values
+(4, 'fish and chips');
+insert into deliveryHasItems values
+(5, 'fish and chips');
+
+insert into delivers values
+(1, 6);
+insert into delivers values
+(2, 7);
+insert into delivers values
+(3, 8);
+insert into delivers values
+(4, 9);
+insert into delivers values
+(5, 10);
+
+insert into storeHasMenus values
+(1,1);
+insert into storeHasMenus values
+(2,2);
+insert into storeHasMenus values
+(3,3);
+insert into storeHasMenus values
+(4,4);
+insert into storeHasMenus values
+(5,5);
+insert into storeHasMenus values
+(1,6);
+insert into storeHasMenus values
+(2,7);
+insert into storeHasMenus values
+(3,8);
+insert into storeHasMenus values
+(4,9);
+insert into storeHasMenus values
+(5,10);
+insert into storeHasMenus values
+(1,11);
+insert into storeHasMenus values
+(2,12);
+insert into storeHasMenus values
+(3,13);
+insert into storeHasMenus values
+(4,14);
+insert into storeHasMenus values
+(5,15);
--- src/phocas/PrivateInfo.java
+++
@@ -1,6 +0,0 @@
-package phocas;
-
-public class PrivateInfo {
-	String account = "ora_n1a0b";
-	String password = "a32109134";
-}
