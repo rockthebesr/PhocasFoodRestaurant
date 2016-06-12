0d890ea6fd42e8a406b6482cd6c22f51d76e50b2
rquong <rquong@users.noreply.github.com>
2016-06-12 13:25:33 -0700

    Update Phocas.java

--- src/phocas/Phocas.java
+++ src/phocas/Phocas.java
@@ -1,5 +1,7 @@
 package phocas;
 
+import javax.swing.plaf.nimbus.State;
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,6 +10,9 @@ import java.sql.Statement;
 
 
 public class Phocas {
+
+    Connection con;
+    Statement stmt;
 
 	public static void main(String[] args) throws SQLException {
 		PrivateInfo p = new PrivateInfo();
@@ -27,6 +32,40 @@ public class Phocas {
 			System.out.println(row.getInt(1));
 		}
 		System.out.println("end");
+
 	}
+
+    public void connect() {
+        PrivateInfo p = new PrivateInfo();
+        // TODO Auto-generated method stub
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            System.out.println("start");
+            Connection con = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@localhost:1522:ug",
+                    p.account,
+                    p.password);
+        } catch (SQLException e) {
+            System.out.println("Failed to connect");
+        }
+    }
+
+    public void update(String s) {
+        try {
+            stmt.executeUpdate(s);
+        } catch (SQLException e) {
+            System.out.println("Update failed");
+        }
+    }
+
+    public ResultSet query(String s) {
+        ResultSet rs = null;
+        try {
+            stmt.executeQuery(s);
+        } catch (SQLException e) {
+            System.out.print("Query failed");
+        }
+        return rs;
+    }
 
 }
